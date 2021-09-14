29eba7d5debab149119efc8ed8ad60084d815032
AurélienMottier <amottier@palo-it.com>
2021-09-14 11:22:54 +0800

    [Trainings] Second exercise to discover recursion with the Fibonacci numbers.

---
+++ 002-recursion-basics-fibonacci-numbers/main/src/fibonacci.ts
@@ -0,0 +1,17 @@
+export default class Fibonacci {
+
+    maximum: number;
+
+    constructor(maximum: number) {
+        this.maximum = maximum;
+    }
+
+    /**
+     * TODO : implement this function to compute all Fibonacci number from 0 to {this.maximum}.
+     */
+    compute(): number[] {
+        // TODO : return the right array... (not this weird array).
+        return [-1];
+    }
+
+}
