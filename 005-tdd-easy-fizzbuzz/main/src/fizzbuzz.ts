ccecba5b8bab62a31555bb964f6582e8e9e3eb46
TinYu <yutin221@gmail.com>
2021-09-19 20:15:38 +0800

    solution by tin

--- 005-tdd-easy-fizzbuzz/main/src/fizzbuzz.ts
+++ 005-tdd-easy-fizzbuzz/main/src/fizzbuzz.ts
@@ -18,7 +18,22 @@ export default class FizzBuzz {
      * - Otherwise add the number to the array.
      */
     computes(): string[] {
-        throw Error("Not implemented yet.");
+        if (this.min > this.max) throw Error("The max must be greater than the min.");
+
+        const numbers: string[] = [];
+        let i;
+        for(i=this.min; i<= this.max; i++) {
+            if (i%3 == 0 && i%5 == 0) {
+                numbers.push("FizzBuzz");
+            } else if(i%3 == 0){
+                numbers.push("Fizz");
+            } else if (i%5 == 0) {
+                numbers.push("Buzz");
+            } else {
+                numbers.push(i.toString());
+            }
+        }
+        return numbers;
     }
 
     /**
@@ -28,7 +43,22 @@ export default class FizzBuzz {
      * - A number is BUZZ if it is divisible by 5 or if it has a 5 in it.
      */
     computesDifferently(): string[] {
-        throw Error("Not implemented yet.");
+        if (this.min > this.max) throw Error("The max must be greater than the min.")
+        const numbers: string[] = [];
+        let i;
+        for(i=this.min; i<= this.max; i++) {
+            if (i%3 == 0 && i%5 == 0) {
+                numbers.push("FizzBuzz");
+            } else if(i%3 == 0 || i.toString().includes("3")){
+                numbers.push("Fizz");
+            } else if (i%5 == 0 || i.toString().includes("5")) {
+                numbers.push("Buzz");
+            } else {
+                numbers.push(i.toString());
+            }
+        }
+        return numbers;
+
     }
 
 }
